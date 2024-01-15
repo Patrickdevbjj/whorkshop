ef5bffc5759bac32ebab240d7dd4ceb5b7b85d42
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-15 17:54:53 -0300

    First commit

---
+++ src/test/java/com/course/UdemyCourse/UdemyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.UdemyCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
