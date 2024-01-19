88d93e83e6822a42d031ebce477b4c6f48a60c82
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-19 20:21:46 -0300

    create User Service

--- src/main/java/com/course/UdemyCourse/config/TestConfig.java
+++ src/main/java/com/course/UdemyCourse/config/TestConfig.java
@@ -1,5 +1,26 @@
 package com.course.UdemyCourse.config;
 
-public class TestConfig {
-    
+import com.course.UdemyCourse.entities.User;
+import com.course.UdemyCourse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1,u2));
+    }
 }
--- src/main/java/com/course/UdemyCourse/resources/UserResource.java
+++ src/main/java/com/course/UdemyCourse/resources/UserResource.java
@@ -1,18 +1,26 @@
 package com.course.UdemyCourse.resources;
 
 import com.course.UdemyCourse.entities.User;
+import com.course.UdemyCourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria","Maria@123","1234","12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
 }
