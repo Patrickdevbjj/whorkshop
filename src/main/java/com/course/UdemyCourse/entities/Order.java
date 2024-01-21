4eaa4044e4a7626620427b54c4982b85fb7f079a
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-20 21:05:22 -0300

    create OrderStatus

--- src/main/java/com/course/UdemyCourse/entities/Order.java
+++ src/main/java/com/course/UdemyCourse/entities/Order.java
@@ -1,5 +1,8 @@
 package com.course.UdemyCourse.entities;
 
+import com.course.UdemyCourse.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -13,7 +16,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
+
+    private int orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -22,9 +29,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id,Instant moment ,User client) {
+    public Order(Long id,Instant moment, OrderStatus orderStatus ,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -42,6 +50,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
---
+++ src/test/java/com/course/UdemyCourse/repositories/OrderRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.course.UdemyCourse.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @Test
+    void deleteInBatch() {
+    }
+}
