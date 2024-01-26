fa585c870f0f57cbb6a6b41cad696a6da5e7bbbb
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-26 19:38:36 -0300

    created Payment class

--- src/main/java/com/course/UdemyCourse/entities/Order.java
+++ src/main/java/com/course/UdemyCourse/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -82,6 +85,14 @@ public class Order implements Serializable {
 
     public void setItems(Set<OrderItem> items) {
         this.items = items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
