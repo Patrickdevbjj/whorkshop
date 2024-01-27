f55311b8b5a8b2ab415d34c026539b11f752f323
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-27 11:45:09 -0300

    add Exception Handler

--- src/main/java/com/course/UdemyCourse/services/UserService.java
+++ src/main/java/com/course/UdemyCourse/services/UserService.java
@@ -2,6 +2,7 @@ package com.course.UdemyCourse.services;
 
 import com.course.UdemyCourse.entities.User;
 import com.course.UdemyCourse.repositories.UserRepository;
+import com.course.UdemyCourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
